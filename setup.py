f1a08a4292c091c03baa547eaedd28d18057d57f
Alireza Roshanzamir <roshanzamir@mahsan.co>
2023-02-16 19:43:06 +0330

    Fix long_description_content_type and license arguments of setuptools.setup.

--- setup.py
+++ setup.py
@@ -17,11 +17,12 @@ class InstallSuitableChromedriverPy(setuptools.command.install.install):
 
 setuptools.setup(
     name="chromedriver-py-auto",
-    version="0.1.0",
+    version="0.1.2",
     description="A wrapper around chromedriver-py library. Detects the Chrome version "
-    "and installs the most suitable chromedriver-py version.",
+                "and installs the most suitable chromedriver-py version.",
     long_description=Path("README.md").read_text(),
-    license=Path("LICENSE").read_text(),
+    long_description_content_type="text/markdown",
+    license="MIT License",
     author="Alireza Roshanzamir",
     author_email="a.roshanzamir1996@gmail.com",
     url="https://github.com/AlirezaRoshanzamir/chromedriver-py-auto",
